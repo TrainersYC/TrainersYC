154f4ddcb1e3d2c4a661c95744c7a24886a55f20
TrainersYC <president.mvtyc@gmail.com>
2025-02-17 00:09:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Handle "Notre Stratégie" button click
+    var strategieBtn = document.getElementById('strategieBtn');
+    strategieBtn.addEventListener('click', function () {
+        window.open('AYC - TYT - Stratégie pour lApprenant Tunisien (1).pdf', '_blank');
+    });
+});
